9fc0b2c4717a118ca36ea541465631b682642a3b
blas377 <bruelop377@g.educaand.es>
2022-01-25 18:11:13 +0100

    parte 1 completa

---
+++ 02-videoclub/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'getHome']);
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/logout', function () {
+    return 'Logout usuario';
+});
+
+Route::get('/catalog', [CatalogController::class, 'getIndex']);
+
+Route::get('/catalog/show/{id}', [CatalogController::class, 'getShow']);
+
+Route::get('/catalog/create', [CatalogController::class, 'getCreate']);
+
+Route::get('/catalog/edit/{id}', [CatalogController::class, 'getEdit']);
